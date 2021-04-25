299daaa1fc1a18c0272ada9d305a924cff9899b7
Chris Zhu <chriszhu@college.harvard.edu>
2021-04-25 18:01:49 -0400

    full send

--- partition.cpp
+++ partition.cpp
@@ -12,9 +12,128 @@
 #include <climits>
 #include <time.h>
 #include <chrono>
-#include <stdlib.h>
+#include <stdlib.h> 
+
+#define KARMAKAR_KARP 0
+#define S_REPEATED_RANDOM 1
+#define S_HILL_CLIMBING 2
+#define S_SIMULATED_ANNEALING 3
+#define P_REPEATED_RANDOM 11
+#define P_HILL_CLIMBING 12
+#define P_SIMULATED_ANNEALING 13
 
 using namespace std;
+
+unsigned long long KK(vector<unsigned long long> a);
+
+class partitions_soln {
+    protected:
+        vector<int> vals; //current solution
+        vector<unsigned long long> nums; // A
+        vector<int> annealing_vals; //S'' in simulated annealing algorithm
+        unsigned long long annealing_residue; 
+        unsigned long long old_residue;
+        int algorithm;
+
+    public:
+        partitions_soln(vector<unsigned long long> A, int algorithm_id) {
+            nums = A;
+            vals = get_random_vector(A.size());
+            annealing_vals = vals;
+            old_residue = get_residue(vals, nums);
+            annealing_residue = old_residue;
+            algorithm = algorithm_id;
+        }
+
+        vector<int> get_random_vector(int length) {
+            vector<int> rand_vector(length, 1);
+            for(int i = 0; i < rand_vector.size(); i++){
+                rand_vector[i] = 1 + (rand() % nums.size());
+            }
+            return rand_vector;
+        }
+        
+        void hill_climb_step() {
+            // change vals
+            int index = rand() % nums.size();
+            int oldval = vals[index];
+            int newval = 1 + (rand() % nums.size());
+            while (newval == oldval){
+                newval = 1 + (rand() % nums.size());
+            }
+            vals[index] = newval;
+
+            unsigned long long new_residue = get_residue(vals, nums);
+            if(old_residue < new_residue) {
+                vals[index] = oldval;
+            } else {
+                old_residue = new_residue;
+            }
+        }
+
+        void repeated_random_step() {
+            vector<int> rand_vector = get_random_vector(nums.size());
+            unsigned long long new_residue = get_residue(rand_vector, nums);
+            if(new_residue <= old_residue){
+                vals = rand_vector;
+                old_residue = new_residue;
+            }
+        }
+        
+        void simulated_annealing_step(int iter) {
+            int index = rand() % nums.size();
+            int oldval = vals[index];
+            int newval = 1 + (rand() % nums.size());
+            while (newval == oldval){
+                newval = 1 + (rand() % nums.size());
+            }
+            vals[index] = newval;
+
+            unsigned long long new_residue = get_residue(vals, nums);
+            // https://stackoverflow.com/questions/686353/random-float-number-generation
+            float p = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+            float t_iter = pow(10, 10) * pow(0.8, floor (iter / 300.));
+            float probability_threshold = exp(-(new_residue - old_residue) / t_iter);
+
+            if((old_residue <= new_residue) && (p > probability_threshold)){
+                vals[index] = oldval;
+            } else { // if we change S
+                old_residue = new_residue;
+                if(new_residue < annealing_residue) {
+                    annealing_residue = new_residue;
+                    annealing_vals = vals;
+                }
+            }
+        }
+
+        static unsigned long long get_residue(vector<int> v, vector<unsigned long long> a) {
+            vector<unsigned long long> new_nums(a.size(), 0);
+            for(int i = 0; i < v.size(); i++) {
+                new_nums[v[i] - 1] += a[i];
+            }
+            return KK(new_nums);
+        }
+
+        void step(int iter) {
+            if(algorithm == P_REPEATED_RANDOM) {
+                repeated_random_step();
+            } else if(algorithm == P_HILL_CLIMBING) {
+                hill_climb_step();
+            } else if(algorithm == P_SIMULATED_ANNEALING) {
+                simulated_annealing_step(iter);
+            }
+        }
+
+        vector<int> get_soln(){
+            return vals;
+        }
+
+        unsigned long long get_final_residue(){
+            return algorithm == P_SIMULATED_ANNEALING ? annealing_residue : old_residue;
+        }
+};
+
+/****************************PARTITION SOLUTION ABOVE HERE****************************/
 
 class signs_soln {
     protected:
@@ -49,10 +168,15 @@ class signs_soln {
                 vals[i2] *= -1;
             }
         }
+
         void hill_climb_step() {
             // change vals
-            int i1 = rand() % 100;
-            int i2 = rand() % 100;
+            int i1 = rand() % nums.size();
+            int i2 = rand() % nums.size();
+            while (i2 == i1) {
+                int i2 = rand() % nums.size();
+            }
+
             int flip_i2 = (rand() % 2) == 0;
             flip_vals(i1, i2, flip_i2);
 
@@ -74,8 +198,12 @@ class signs_soln {
         }
         
         void simulated_annealing_step(int iter) {
-            int i1 = rand() % 100;
-            int i2 = rand() % 100;
+            int i1 = rand() % nums.size();
+            int i2 = rand() % nums.size();
+            while (i2 == i1) {
+                int i2 = rand() % nums.size();
+            }
+
             int flip_i2 = (rand() % 2) == 0;
             flip_vals(i1, i2, flip_i2);
 
@@ -94,17 +222,6 @@ class signs_soln {
                     annealing_vals = vals;
                 }
             }
-
-        }
-
-        void step(int iter) {
-            if(algorithm == 1) {
-                repeated_random_step();
-            } else if(algorithm == 2) {
-                hill_climb_step();
-            } else if(algorithm == 3) {
-                simulated_annealing_step(iter);
-            }
         }
 
         static unsigned long long get_residue(vector<int> v, vector<unsigned long long> a) {
@@ -112,15 +229,25 @@ class signs_soln {
             for(int i = 0; i < v.size(); i++) {
                 residue += v[i] * a[i];
             }
-            return (unsigned long long) llabs(residue);
+            return llabs(residue);
+        }
+
+        void step(int iter) {
+            if(algorithm == S_REPEATED_RANDOM) {
+                repeated_random_step();
+            } else if(algorithm == S_HILL_CLIMBING) {
+                hill_climb_step();
+            } else if(algorithm == S_SIMULATED_ANNEALING) {
+                simulated_annealing_step(iter);
+            }
         }
 
         vector<int> get_soln(){
             return vals;
         }
 
-        unsigned long long get_residue(){
-            return algorithm == 3 ? annealing_residue : old_residue;
+        unsigned long long get_final_residue(){
+            return algorithm == S_SIMULATED_ANNEALING ? annealing_residue : old_residue;
         }
 };
 
@@ -237,14 +364,20 @@ int main(int argc, char *argv[]){
 
     file.close();
 
-    if(algorithm == 0) {
+    if(algorithm == KARMAKAR_KARP) {
         cout << KK(init) << endl;     
     } else if(algorithm < 10){
         signs_soln soln = signs_soln(init, algorithm);
         for(int i = 0; i < 25000; i++) {
             soln.step(i);
         }
-        cout << soln.get_residue() << endl;     
+        cout << soln.get_final_residue() << endl;     
+    } else {
+        partitions_soln soln = partitions_soln(init, algorithm);
+        for(int i = 0; i < 25000; i++) {
+            soln.step(i);
+        }
+        cout << soln.get_final_residue() << endl;     
     }
     // signs_soln hill_soln = signs_soln(init);
     // signs_soln rep_soln = signs_soln(init);
